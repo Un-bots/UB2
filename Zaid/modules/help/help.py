d7e3df321ebe8af8f7f54657bf5cce48a65e346c
Un-bots <161623523+Un-bots@users.noreply.github.com>
2024-03-02 12:37:02 +0530

    Update help.py

--- Zaid/modules/help/help.py
+++ Zaid/modules/help/help.py
@@ -17,40 +17,6 @@ async def edit_or_reply(message: Message, *args, **kwargs) -> Message:
     )
     return await xyz(*args, **kwargs)
 
-@Client.on_message(filters.command(["help", "helpme"], ".") & filters.me)
-async def module_help(client: Client, message: Message):
-    cmd = message.command
-    help_arg = ""
-    bot_username = (await app.get_me()).username
-    if len(cmd) > 1:
-        help_arg = " ".join(cmd[1:])
-    elif not message.reply_to_message and len(cmd) == 1:
-        await message.edit("☁️")
-        try:
-            nice = await client.get_inline_bot_results(bot=bot_username, query="helper")
-            await asyncio.gather(
-                message.delete(),
-                client.send_inline_bot_result(
-                    message.chat.id, nice.query_id, nice.results[0].id
-                ),
-            )
-        except BaseException as e:
-            print(f"{e}")
-            ac = PrettyTable()
-            ac.header = False
-            ac.title = "𝗨𝗡 𝗕𝗢𝗧"
-            ac.align = "l"
-            for x in split_list(sorted(CMD_HELP.keys()), 2):
-                ac.add_row([x[0], x[1] if len(x) >= 2 else None])
-            xx = await client.send_message(
-                message.chat.id,
-                f"```{str(ac)}```\n• @un_bots × @un_bots_info •",
-                reply_to_message_id=ReplyCheck(message),
-            )
-            await xx.reply(
-                f"**Usage:** `.help broadcast` **To View Module Information**"
-            )
-            return
 
     if help_arg:
         if help_arg in CMD_HELP:
