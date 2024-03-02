8919711febe526b5873408177dd31b0bd4a49220
Un-bots <161623523+Un-bots@users.noreply.github.com>
2024-03-02 11:40:02 +0530

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ api = Api(app)
 
 class Greeting (Resource):
     def get(self):
-        return "Zaid Userbot is Up & Running!"
+        return "Un Userbot is Up & Running!"
 
 api.add_resource(Greeting, '/')
 app.run(host="0.0.0.0", port=os.environ.get("PORT", 8080))
